571c1df3620f10b507fedc4142b0d2745b9be3c1
Dustin <dmkelly100@gmail.com>
2020-03-16 13:06:51 -0400

    working on card flip

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-	const gameApp = {};
+	const cardGameApp = {};
+
+	cardGameApp.cards = [1, 1, 2, 2, 3, 3];
+
+	$('.card').on('click', function() {
+		$(this).toggleClass('cardFlip');
+	});
 });
