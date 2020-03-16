b69c76f6ee28ea1ad1be0daef3347ad02e1f953c
Dustin <dmkelly100@gmail.com>
2020-03-16 15:27:20 -0400

    saving

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,27 @@ $(document).ready(function() {
 
 	cardGameApp.cards = [1, 1, 2, 2, 3, 3];
 
+    // on click toggles class cardFlip and gives it the card flipping effect
+
 	$('.card').on('click', function() {
 		$(this).toggleClass('cardFlip');
 		console.log(this);
 		console.log($(this).attr('data-card'));
-	});
+    });
+
+    cardGameApp.isMatch = function() {
+        if ($(this).attr('data-card') === $(this).attr('data-card')) {
+            console.log('match')
+        }else {
+            console.log('try again')
+        }
+    }
+    
+
+
+
+
+
+
+
 });
