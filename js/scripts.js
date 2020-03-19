918f09e5ecd86288bd0573cb862a677b1ff02e5c
Dustin <dmkelly100@gmail.com>
2020-03-19 12:19:19 -0400

    saving small changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,4 @@
 $(document).ready(function() {
-	const cardGameApp = {};
-
 	let firstClick = '';
 	let secondClick = '';
 	let hasBeenFlipped = false;
@@ -11,22 +9,25 @@ $(document).ready(function() {
 		if (hasBeenFlipped === false) {
 			$(this).toggleClass('cardFlip');
 			hasBeenFlipped = true;
+
 			// sets variable on first click
-			firstClick = $(this).attr('data-card');
+			firstClick = $(this);
 			console.log(firstClick + 'clickOne');
 		} else {
 			$(this).toggleClass('cardFlip');
 			hasBeenFlipped = false;
 			// sets variable on second click
-			secondClick = $(this).attr('data-card');
+			secondClick = $(this);
 			console.log(secondClick + 'clickTwo');
 
+			// if cards are not a match cards will wait 1 sec and flip back over.
 			setTimeout(doesItMatch, 1000);
 		}
 	});
 
+	// function that checks if data types are the same
 	const doesItMatch = function() {
-		if (firstClick === secondClick) {
+		if (firstClick.attr('data-card') === secondClick.attr('data-card')) {
 			console.log(`it's a match`);
 		} else {
 			console.log(`try again`);
@@ -34,7 +35,13 @@ $(document).ready(function() {
 		}
 	};
 
+	// Resets cards if they don't match
 	const resetCardsIfNotMatch = function() {
 		$('.card').removeClass('cardFlip');
 	};
+
+	// play button that starts the game
+	$('.playButton').on('click', function() {
+		$('header').fadeOut('slow');
+	});
 });
